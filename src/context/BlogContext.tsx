7abdddc48d0984f3e52087a4938512b534d2456a
Abid Ali <abdsoftfsd@gmail.com>
2024-01-24 11:44:40 +0500

    Refactors the code to useReducer in BlogContext.tsx

--- src/context/BlogContext.tsx
+++ src/context/BlogContext.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import {Context, useState} from 'react';
-import {IBlogContext} from '../types/interfaces.ts';
+import {Context, useReducer} from 'react';
+import {IBlog, IBlogContext} from '../types/interfaces.ts';
 
 const defaultValue: IBlogContext = {
   data: [],
@@ -9,21 +9,30 @@ const defaultValue: IBlogContext = {
 
 const BlogContext: Context<IBlogContext> = React.createContext(defaultValue);
 
+interface IAction {
+  type: string;
+  payload: any;
+}
+
+const blogReducer = (state: IBlog[], action: IAction) => {
+  switch (action.type) {
+    case 'add':
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+};
+
 export interface IBlogProviderProps {
   children: any;
 }
+
 const BlogProvider = ({children}: IBlogProviderProps) => {
-  const [blogPosts, setBlogPost] = useState<Array<any>>([]);
+  const [blogPosts, dispatch] = useReducer(blogReducer, []);
 
   const addBlogPost = () => {
     const indexNumber = blogPosts.length + 1;
-    setBlogPost([
-      ...blogPosts,
-      {
-        title: `Blog Post #${indexNumber}`,
-        body: `Blog Post Body ${indexNumber}`,
-      },
-    ]);
+    dispatch({type: 'add', payload: {title: `BlogPost ${indexNumber}`, body: `Body Post ${indexNumber}`}});
   };
 
   return <BlogContext.Provider value={{data: blogPosts, addBlogPost}}>{children}</BlogContext.Provider>;
